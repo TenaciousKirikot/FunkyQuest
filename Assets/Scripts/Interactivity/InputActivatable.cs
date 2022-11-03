c09329e44112e2b62992db4c527ba4815450f7ff
TenaciousKirikot <kirillthegeneralissimus@gmail.com>
2022-11-04 00:21:10 +0700

    Updated the Room 1
    Added some improvements to the code, alongside remaking the entire scene.

---
+++ Assets/Scripts/Interactivity/InputActivatable.cs
@@ -0,0 +1,29 @@
+﻿
+using TMPro;
+using UnityEngine;
+
+namespace FunkyQuest
+{
+    internal class InputActivatable : Activatable
+    {
+        [Header("Input")]
+        [SerializeField]    private string          _acceptedString;
+        [SerializeField]    private TMP_InputField  _inputField;
+
+        private void Start()
+        {
+            if (_inputField != null)
+            {
+                _inputField.onValueChanged.AddListener(
+                    value =>
+                    {
+                        if (value.ToLower() == _acceptedString.ToLower() && !HasActivated)
+                        {
+                            Activate();
+                        }
+                    }
+                );
+            }
+        }
+    }
+}
